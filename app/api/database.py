fe1514d23b3b6dc5d31caf19251ad471dd9e067c
Liam <zhegalo200405@gmail.com>
2026-07-02 17:14:01 +0300

    Fix database connection for Render

--- app/api/database.py
+++ app/api/database.py
@@ -5,14 +5,16 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-DB_USER = os.getenv("POSTGRES_USER")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-DB_NAME = os.getenv("POSTGRES_DB")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DB_HOST = os.getenv("DB_HOST", "localhost") 
-DB_PORT = os.getenv("DB_PORT", "5432")
-
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+if not DATABASE_URL:
+    DB_USER = os.getenv("POSTGRES_USER")
+    DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    DB_NAME = os.getenv("POSTGRES_DB")
+    DB_HOST = os.getenv("DB_HOST", "localhost") 
+    DB_PORT = os.getenv("DB_PORT", "5432")
+    
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
